3684d471d69fd099ad8c926cd09604e9a541f435
Li Jie <lijie@applean.cn>
2016-06-23 15:35:31 +0800

    Add active_admin

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
