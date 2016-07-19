e21e65955f16b82d7e0f651dfd60fb354e94bd10
bob <bob1993.dev@gmail.com>
2016-07-19 14:46:43 +0800

    add genSubscribedData in dataHelper

--- GmtcClient/src/helper/dataHelper.js
+++ GmtcClient/src/helper/dataHelper.js
@@ -1,4 +1,4 @@
-export default function (data) {
+export function genData (data) {
   return data.days.map(day => {
     let topicTimeMap = {}
     day.rooms.forEach(room => {
@@ -19,20 +19,42 @@ export default function (data) {
   })
 }
 
+/*
+ * 潜在Bug: 组合数据时，一定要返回一个新的对象。(days是一个不可变的源数据)
+ * 否则，第一次调用，ListView接收到的是被修改了的原始数据。后期则是一直使用对原始数据的修改。
+ * 因此，在每次调用的时候，一定要返回一个新的对象，以供ListView的WillReceiveProps做对比
+ */
 export function combineData (days, subscription) {
   return days.map(day => {
     const newTopics = {}
     for (let sectionID in day.topics) {
       let section = day.topics[sectionID].map(
-        topic => {
-          return {
-            ...topic,
-            isSubscribed: subscription.indexOf(topic.id) > -1
-          }
+        topic => ({
+          ...topic,
+          isSubscribed: subscription.indexOf(topic.id) > -1
         })
-      day.topics[sectionID] = section
+      )
       newTopics[sectionID] = section
     }
     return {...day, topics: newTopics}
   })
+}
+
+// [{..., topics: {1: [], 2: []}}, {}]
+export function genSubscribedData (days, subscription) {
+  let result = {}
+  days.forEach(day => {
+    for (let sectionID in day.topics) {
+      day.topics[sectionID].forEach(topic => {
+        if (subscription.indexOf(topic.id) > -1) {
+          if (!result[day.id]) {
+            result[day.id] = []
+          }
+          result[day.id].push(topic)
+        }
+      })
+    }
+  })
+  console.log('result: ', result)
+  return result
 }
