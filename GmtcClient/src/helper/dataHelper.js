16a65b6991d001a5c70c42f44696add821aa6548
poberWong <poberwong@gmail.com>
2016-07-22 13:47:58 +0800

    using cycle data-structure will crash in persist-storage

--- GmtcClient/src/helper/dataHelper.js
+++ GmtcClient/src/helper/dataHelper.js
@@ -2,12 +2,13 @@ export function genData (data) {
   return data.days.map(day => {
     let topicTimeMap = {}
     day.rooms.forEach(room => {
-      room.topics.forEach(topic => {
+      const {topics, ...others} = room
+      topics.forEach(topic => {
         if (!topicTimeMap[topic.start_at]) {
           topicTimeMap[topic.start_at] = []
         }
         if (!(topic.rest && topicTimeMap[topic.start_at].length === 1)) {
-          topic.room = room
+          topic.room = {...others}
           topic.isSubscribed = false
           topicTimeMap[topic.start_at].push(topic)
         }
